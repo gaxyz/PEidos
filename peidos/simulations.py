dd872f20c84eec0f27655c959a9aae9bd876b107
Gaston Rijo <gaston.rijo@gmail.com>
2020-09-01 12:20:15 -0300

    Added migration pulse option

--- peidos/simulations.py
+++ peidos/simulations.py
@@ -11,6 +11,7 @@ import os
 from pathlib import Path
 import subprocess
 
+
 def treelike_neutral(tree, d, script_file ): 
     """
     Write a very specific simulation scheme (tree-like, migration).
@@ -19,19 +20,19 @@ def treelike_neutral(tree, d, script_file ):
     d is a configuration dictionary with simulation parameters.
     Script file is the output name of the file.
     """    
-    slim_script = []
+    slim_script = ee.SlimScript()
 
     # Frequently used parameters
     popsize = d["simulation_popsize"]
 
     # Initialize
-    slim_script.append( ee.initialize(d["mutation_rate"],
-                                   d["recombination_rate"],
-                                   d["genome_size"])  )
+    slim_script.initialize(d["mutation_rate"],
+                           d["recombination_rate"],
+                           d["genome_size"])  
     # Setup
-    slim_script.append( ee.setup(d["pop_size_filename"],
-                              tree.seed_node.slabel,
-                              popsize))    
+    slim_script.setup(d["pop_size_filename"],
+                      tree.seed_node.slabel,
+                      popsize)    
     
     # Traverse tree and write splits
     for node in tree.preorder_node_iter():
@@ -47,22 +48,84 @@ def treelike_neutral(tree, d, script_file ):
             # For every child that is not the source population, write a split
             for child in childs:
                 if source_pop != child.slabel:
-                    slim_script.append( ee.split(child.slabel,
-                                                 source_pop,
-                                                 generation,
-                                                 popsize) )
+                    slim_script.split( child.slabel,
+                                       source_pop,
+                                       generation,
+                                       popsize )
     # End simulation
     total_tree_length = d["burnin_time"] + int(tree.length()) # This is total number of generations
-    slim_script.append( ee.end_simulation(total_tree_length,
-                                       tree.leaves,
-                                       d["output_sample_size"]) )
-    # Join script
-    s = "\n////////////////////////\n".join(slim_script)
+    slim_script.end_simulation(total_tree_length,
+                               tree.leaves,
+                               d["output_sample_size"]) 
     
-    with open(script_file, 'w') as handle:
-        handle.write(s)
+    slim_script.write_script( script_file )
     
 
+
+
+
+def migration_pulse_neutral(tree, d, script_file ): 
+    """
+    Write a very specific simulation scheme (tree-like, migration).
+    
+    Tree is properly labeled DendroPy tree (use read_tree()).
+        d is a configuration dictionary with simulation parameters.
+    Script file is the output name of the file.
+    """    
+    slim_script = ee.SlimScript()
+
+    # Frequently used parameters
+    popsize = d["simulation_popsize"]
+
+    # Initialize
+    slim_script.initialize(d["mutation_rate"],
+                           d["recombination_rate"],
+                           d["genome_size"])  
+    # Setup
+    slim_script.setup(d["pop_size_filename"],
+                      tree.seed_node.slabel,
+                      popsize)    
+    
+    # Traverse tree and write splits
+    for node in tree.preorder_node_iter():
+        
+        # If node has child nodes (omit leaves)
+        if node.child_nodes():
+            # List child nodes
+            childs = node.child_nodes()
+            # Tag source population
+            source_pop = node.slabel
+            # Tag generation of split
+            generation = node.generation
+            # For every child that is not the source population, write a split
+            for child in childs:
+                if source_pop != child.slabel:
+                    slim_script.split( child.slabel,
+                                       source_pop,
+                                       generation,
+                                       popsize )
+    
+    # Write migration pulse
+    
+    slim_script.admixture_pulse(d["pulse_destination"],
+                                d["pulse_source"],
+                                d["pulse_rate"],
+                                d["pulse_generation"])
+    
+    
+    
+    # End simulation
+    total_sim_generations = d["pulse_generation"] + d["post_admixture_time"]  # This is total number of generations
+    total_sim_generations = int(total_sim_generations)
+    slim_script.end_simulation(total_sim_generations,
+                               tree.leaves,
+                               d["output_sample_size"]) 
+    
+    slim_script.write_script( script_file )
+    
+    
+    
+    
 
 def slimsim(script_file, seed, outdir,tag):
     
