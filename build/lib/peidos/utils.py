1ee9a0bfc501dd122ceb6be3c042d33850046f0f
Gaston Rijo <gaston.rijo@gmail.com>
2020-09-09 17:35:32 -0300

    Keep outgroup build 3

--- build/lib/peidos/utils.py
+++ build/lib/peidos/utils.py
@@ -331,16 +331,14 @@ def run_hapFLK(file_prefix,outfile_prefix,d):
              "--prefix",outfile_prefix,
              "--outgroup", "p1",
              "-K", K , "--nfit", nfit,
-             "--kinship", covariance_file,
-             "--keep-outgroup"]
+             "--kinship", covariance_file]
     else:
         s = ["hapflk", "--ncpu", ncpu,
              "--reynolds-snps", reynold_snps,
              "--bfile", file_prefix,
              "--prefix",outfile_prefix,
              "--outgroup", "p1",
-             "-K", K , "--nfit", nfit,
-             "--keep-outgroup"]
+             "-K", K , "--nfit", nfit]
         
     
             
